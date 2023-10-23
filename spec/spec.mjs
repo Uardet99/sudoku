ef8cc48bd13bac0f8cb6799f44dbef6dc101b87f
Edu <79867404+Uardet99@users.noreply.github.com>
2023-10-23 16:55:14 +0200

    second commit

---
+++ spec/spec.mjs
@@ -0,0 +1,11 @@
+import { createArray } from "../src/functions.js";
+
+describe("Array", function () {
+  describe("createArray", function () {
+    if('Should return [0,0,0]', function() {
+        expect()
+    }) {
+
+    }
+  });
+});
